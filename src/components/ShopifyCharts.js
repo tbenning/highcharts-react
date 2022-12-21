71880414144ac209a754eacefe97a1fa51eb1bb1
Tyler Benning <tbenning@github.com>
2022-12-20 22:15:49 -0800

    fix shopify css

--- src/components/ShopifyCharts.js
+++ src/components/ShopifyCharts.js
@@ -1,129 +1,131 @@
 import { PolarisVizProvider, BarChart } from '@shopify/polaris-viz'
-
+import '@shopify/polaris-viz/build/esm/styles.css'
 // Polaris Data Viz Lib
 // https://github.com/Shopify/polaris-viz
 
 export default function ShopifyCharts() {
     return (
-        <div
-            style={{
-                height: '1000px',
-            }}
-        >
-            <PolarisVizProvider
-                themes={{
-                    Default: {
-                        chartContainer: {
-                            padding: '20px',
-                        },
-                    },
+        <div>
+            <div
+                style={{
+                    height: '500px',
+                    marginBottom: '50px',
                 }}
             >
-                <BarChart
-                    theme="Light"
-                    data={[
-                        {
-                            data: [
-                                {
-                                    key: 'Monday',
-                                    value: 3,
-                                },
-                                {
-                                    key: 'Tuesday',
-                                    value: -7,
-                                },
-                                {
-                                    key: 'Wednesday',
-                                    value: -7,
-                                },
-                                {
-                                    key: 'Thursday',
-                                    value: -8,
-                                },
-                                {
-                                    key: 'Friday',
-                                    value: 50,
-                                },
-                                {
-                                    key: 'Saturday',
-                                    value: 0,
-                                },
-                                {
-                                    key: 'Sunday',
-                                    value: 0.1,
-                                },
-                            ],
-                            name: 'Breakfast',
+                <PolarisVizProvider
+                    themes={{
+                        Default: {
+                            chartContainer: {
+                                padding: '20px',
+                            },
                         },
-                        {
-                            data: [
-                                {
-                                    key: 'Monday',
-                                    value: 4,
-                                },
-                                {
-                                    key: 'Tuesday',
-                                    value: 0,
-                                },
-                                {
-                                    key: 'Wednesday',
-                                    value: -10,
-                                },
-                                {
-                                    key: 'Thursday',
-                                    value: 15,
-                                },
-                                {
-                                    key: 'Friday',
-                                    value: 8,
-                                },
-                                {
-                                    key: 'Saturday',
-                                    value: 50,
-                                },
-                                {
-                                    key: 'Sunday',
-                                    value: 0.1,
-                                },
-                            ],
-                            name: 'Lunch',
-                        },
-                        {
-                            data: [
-                                {
-                                    key: 'Monday',
-                                    value: 7,
-                                },
-                                {
-                                    key: 'Tuesday',
-                                    value: 0,
-                                },
-                                {
-                                    key: 'Wednesday',
-                                    value: -15,
-                                },
-                                {
-                                    key: 'Thursday',
-                                    value: -12,
-                                },
-                                {
-                                    key: 'Friday',
-                                    value: 50,
-                                },
-                                {
-                                    key: 'Saturday',
-                                    value: 5,
-                                },
-                                {
-                                    key: 'Sunday',
-                                    value: 0.1,
-                                },
-                            ],
-                            name: 'Dinner',
-                        },
-                    ]}
-                />
-                {/* <BarChart
+                    }}
+                >
+                    <BarChart
+                        theme="Light"
+                        data={[
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 3,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: -7,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -7,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: -8,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Breakfast',
+                            },
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 4,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -10,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: 15,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 8,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Lunch',
+                            },
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 7,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -15,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: -12,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 5,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Dinner',
+                            },
+                        ]}
+                    />
+                    {/* <BarChart
                     data={[
                         {
                             data: [
@@ -226,7 +228,231 @@ export default function ShopifyCharts() {
                         },
                     ]}
                 /> */}
-            </PolarisVizProvider>
+                </PolarisVizProvider>
+            </div>
+            <div
+                style={{
+                    height: '500px',
+                    marginBottom: '50px',
+                }}
+            >
+                <PolarisVizProvider
+                    themes={{
+                        Default: {
+                            chartContainer: {
+                                padding: '20px',
+                            },
+                        },
+                    }}
+                >
+                    <BarChart
+                        data={[
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 3,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: -7,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -7,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: -8,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Breakfast',
+                            },
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 4,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -10,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: 15,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 8,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Lunch',
+                            },
+                            {
+                                data: [
+                                    {
+                                        key: 'Monday',
+                                        value: 7,
+                                    },
+                                    {
+                                        key: 'Tuesday',
+                                        value: 0,
+                                    },
+                                    {
+                                        key: 'Wednesday',
+                                        value: -15,
+                                    },
+                                    {
+                                        key: 'Thursday',
+                                        value: -12,
+                                    },
+                                    {
+                                        key: 'Friday',
+                                        value: 50,
+                                    },
+                                    {
+                                        key: 'Saturday',
+                                        value: 5,
+                                    },
+                                    {
+                                        key: 'Sunday',
+                                        value: 0.1,
+                                    },
+                                ],
+                                name: 'Dinner',
+                            },
+                        ]}
+                    />
+                    {/* <BarChart
+                    data={[
+                        {
+                            data: [
+                                {
+                                    key: 'Monday',
+                                    value: 3,
+                                },
+                                {
+                                    key: 'Tuesday',
+                                    value: -7,
+                                },
+                                {
+                                    key: 'Wednesday',
+                                    value: -7,
+                                },
+                                {
+                                    key: 'Thursday',
+                                    value: -8,
+                                },
+                                {
+                                    key: 'Friday',
+                                    value: 50,
+                                },
+                                {
+                                    key: 'Saturday',
+                                    value: 0,
+                                },
+                                {
+                                    key: 'Sunday',
+                                    value: 0.1,
+                                },
+                            ],
+                            name: 'Breakfast',
+                        },
+                        {
+                            data: [
+                                {
+                                    key: 'Monday',
+                                    value: 4,
+                                },
+                                {
+                                    key: 'Tuesday',
+                                    value: 0,
+                                },
+                                {
+                                    key: 'Wednesday',
+                                    value: -10,
+                                },
+                                {
+                                    key: 'Thursday',
+                                    value: 15,
+                                },
+                                {
+                                    key: 'Friday',
+                                    value: 8,
+                                },
+                                {
+                                    key: 'Saturday',
+                                    value: 50,
+                                },
+                                {
+                                    key: 'Sunday',
+                                    value: 0.1,
+                                },
+                            ],
+                            name: 'Lunch',
+                        },
+                        {
+                            data: [
+                                {
+                                    key: 'Monday',
+                                    value: 7,
+                                },
+                                {
+                                    key: 'Tuesday',
+                                    value: 0,
+                                },
+                                {
+                                    key: 'Wednesday',
+                                    value: -15,
+                                },
+                                {
+                                    key: 'Thursday',
+                                    value: -12,
+                                },
+                                {
+                                    key: 'Friday',
+                                    value: 50,
+                                },
+                                {
+                                    key: 'Saturday',
+                                    value: 5,
+                                },
+                                {
+                                    key: 'Sunday',
+                                    value: 0.1,
+                                },
+                            ],
+                            name: 'Dinner',
+                        },
+                    ]}
+                /> */}
+                </PolarisVizProvider>
+            </div>
         </div>
     )
 }
